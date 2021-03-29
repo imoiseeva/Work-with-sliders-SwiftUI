8f90dc0b3ee593bf591373c8aa5de0504eaa4fa5
imoiseeva <moiseeva.irina@gmail.com>
2021-03-29 15:08:58 +0300

    Initial Commit

---
+++ Work with sliders SwiftUI/Work_with_sliders_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_with_sliders_SwiftUIApp.swift
+//  Work with sliders SwiftUI
+//
+//  Created by Irina Moiseeva on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Work_with_sliders_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
